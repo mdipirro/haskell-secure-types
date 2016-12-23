9fa88d8b50ec09d160cdaaae1e21f3ff59e1bf4a
mdipirro <matteodipirro@gmail.com>
2016-12-23 18:28:39 +0100

    Align =

--- app/Validation/Employee.hs
+++ app/Validation/Employee.hs
@@ -8,20 +8,20 @@ dateRegex   = "(^(((0[1-9]|1[0-9]|2[0-8])[/](0[1-9]|1[012]))|((29|30|31)[/](0[13
 emailRegex  = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
 
 validateDate :: [Employee] -> Bool
-validateDate []                         = True
+validateDate []                           = True
 validateDate ((Employee f l b s e ml):es) = rd && validateDate es
-                                          where rd = b =~ dateRegex
+                                            where rd = b =~ dateRegex
 
 validateName :: [Employee] -> Bool
-validateName []                         = True
+validateName []                           = True
 validateName ((Employee f l b s e ml):es) = rf && rl && validateName es
-                                          where rf = f =~ nameRegex
-                                                rl = l =~ nameRegex
+                                            where rf = f =~ nameRegex
+                                                  rl = l =~ nameRegex
 
 validateEmail :: [Employee] -> Bool
-validateEmail []                         = True
-validateEmail ((Employee f l b s e ml):es) =  re && validateEmail es
-                                            where re = e =~ emailRegex
+validateEmail []                            = True
+validateEmail ((Employee f l b s e ml):es)  = re && validateEmail es
+                                              where re = e =~ emailRegex
 
 validate :: [[Employee] -> Bool]
 validate = [validateName, validateDate, validateEmail]
