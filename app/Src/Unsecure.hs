0f63f95f2e3e54e7add0a3d633a7a2bf4047fc2f
mdipirro <matteodipirro@gmail.com>
2016-12-26 19:26:32 +0100

    Add transform function

--- app/Src/Unsecure.hs
+++ app/Src/Unsecure.hs
@@ -1,16 +1,18 @@
-module Src.Unsecure (Unsecure, makeUnsecure, extract) where
+module Src.Unsecure (Unsecure, makeUnsecure, validate, transform) where
 
---type ValidationError a = a
 data Unsecure a b = Unsecure ([a -> Maybe b], a)
 
-extract :: Unsecure a b -> Either a [b]
-extract (Unsecure (fs, v)) =  if null errors
+validate :: Unsecure a b -> Either a [b]
+validate (Unsecure (fs, v)) = if null errors
                               then Left v
                               else Right errors
                               where maybes = map (\f -> f v) fs
                                     justs  = filter (\m -> case m of  Nothing -> False
                                                                       Just _  -> True) maybes
                                     errors = map (\(Just m) -> m) justs
+
+transform :: Unsecure a b -> (a -> a) -> Unsecure a b
+transform (Unsecure (fs, v)) f = Unsecure (fs, f v)
 
 makeUnsecure :: a -> [a -> Maybe b] -> Unsecure a b
 makeUnsecure value fs = Unsecure (fs, value)
