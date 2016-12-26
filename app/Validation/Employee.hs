550bf12eedfe4e9ce7ec2d3ef5b373d8f20a3f1e
mdipirro <matteodipirro@gmail.com>
2016-12-26 19:27:16 +0100

    Refactor functions and add salary validation

--- app/Validation/Employee.hs
+++ app/Validation/Employee.hs
@@ -3,38 +3,42 @@ module Validation.Employee (validate, EmployeeError(Birthdate, Name, Email)) whe
 import Text.Regex.Posix
 import Text.Email.Validate (isValid)
 import Data.ByteString.Char8 (pack)
+import Data.List (partition)
 import Src.Employee
---import Src.Unsecure
 
-data EmployeeError =  Birthdate String | Name String | Email String
+data EmployeeError =  Birthdate String | Name String | Email String | Salary String
                       deriving Show
 
 nameRegex   = "^[a-zA-Z =]*$"
 dateRegex   = "(^(((0[1-9]|1[0-9]|2[0-8])[/](0[1-9]|1[012]))|((29|30|31)[/](0[13578]|1[02]))|((29|30)[/](0[4,6,9]|11)))[/](19|[2-9][0-9])[0-9][0-9]$)|(^29[/]02[/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)"
 
+validationFold :: [Employee] -> (Employee -> Bool) -> Maybe Employee
+validationFold [] _     = Nothing
+validationFold (e:es) p = if p e
+                          then validationFold es p
+                          else Just e
+
 validateDate :: [Employee] -> Maybe EmployeeError
-validateDate []     = Nothing
-validateDate (e:es) = if b =~ dateRegex
-                      then validateDate es
-                      else Just (Birthdate ("Incorrect date " ++ b))
-                      where b = birthdate e
+validateDate es = case validationFold es (\e -> birthdate e =~ dateRegex) of
+                    Nothing -> Nothing
+                    Just e  -> Just (Birthdate ("Incorrect date " ++ birthdate e))
 
 validateName :: [Employee] -> Maybe EmployeeError
-validateName []     = Nothing
-validateName (e:es) = if rf && rl
-                      then validateName es
-                      else Just (Name ("Incorrect name " ++ f ++ " " ++ l))
-                      where f   = firstName e
-                            l   = lastName e
-                            rf  = f =~ nameRegex
-                            rl  = l =~ nameRegex
+validateName es = case validationFold es (\e -> firstName e =~ nameRegex && lastName e =~ nameRegex) of
+                    Nothing -> Nothing
+                    Just e  -> Just (Name ("Incorrect name " ++ (firstName e) ++ " " ++ (lastName e)))
 
 validateEmail :: [Employee] -> Maybe EmployeeError
-validateEmail []      = Nothing
-validateEmail (e:es)  = if isValid (pack email')
-                        then validateEmail es
-                        else Just (Email ("Invalid email " ++ email'))
-                        where email' = email e
+validateEmail es =  case validationFold es (\e -> isValid . pack . email $ e) of
+                      Nothing -> Nothing
+                      Just e  -> Just (Email ("Invalid email " ++ (email e)))
+
+salaryLowerThanLeaders :: [Employee] -> Maybe EmployeeError
+salaryLowerThanLeaders es = case validationFold nls (\nl -> length (filter (\l -> salary l < salary nl) ls) == 0) of
+                              Nothing -> Nothing
+                              Just e  -> Just (Salary (firstName e ++ " " ++ (lastName e) ++ "'s salary is too high"))
+                            where (ls, nls) = partition (\e -> leader e) es
+
 
 validate :: [[Employee] -> Maybe EmployeeError]
-validate = [validateDate, validateName, validateEmail]
+validate = [validateDate, validateName, validateEmail, salaryLowerThanLeaders]
