28acc9b91f80b8e90a8b6251d05daea333fa90b0
mdipirro <matteodipirro@gmail.com>
2016-12-26 19:27:25 +0100

    Tests

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,11 @@ import Src.Employee
 import Src.Unsecure
 import Src.JSONHandler
 
+import Data.List (partition)
+
 main :: IO ()
 main = do employees <- getEmployees
-          let e = extract employees
-          print e
+          let employees' = transform employees (\es -> map (increaseSalary 200) es)
+          let e = validate employees
+          case e of Left emp    -> print "OK"
+                    Right errs  -> print errs
