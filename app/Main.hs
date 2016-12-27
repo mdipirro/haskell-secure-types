7c1bc96861b63daccd874c451ad1ad018fd692a1
mdipirro <matteodipirro@gmail.com>
2016-12-27 10:20:09 +0100

    Add extract function

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 import Src.Model.Employee
 import Src.IO.JSONHandler
 import Security.Unsecure
-import qualified Security.Taint as T
+import Security.Taint
 
 import Data.List (partition)
 
@@ -9,5 +9,6 @@ main :: IO ()
 main = do employees <- getEmployees
           let employees'  = transform employees (\es -> map (increaseSalary 200) es)
               e           = validate employees
-              t           = pure 10 :: T.Taint Int
-          print $ T.value t 0
+              t           = pure (\x y -> x + y) <*> pure 10 <*> pure 20 :: Taint Int
+              t'          = pure (\x y z -> x + y + z) <*> pure 30 <*> t <*> pure 10
+          print $ get t'
--- app/Security/Taint.hs
+++ app/Security/Taint.hs
@@ -1,14 +1,19 @@
-module Security.Taint (Taint, fmap, pure, (<*>), (>>=), value) where
+module Security.Taint (Taint, fmap, pure, (<*>), (>>=), get) where
 
 type State   = Int
 newtype ST a  = S (State -> (a, State))
 type Taint a  = ST a
 
-value :: ST a -> State -> a
-value s x = fst $ app s x
+
+get :: ST a -> Maybe a
+get s = if st < 5
+        then Just v
+        else Nothing
+        where (v, st) = app s 0
 
 app :: ST a -> State -> (a, State)
-app (S st) x = st x
+app (S st) x =  (a, s + 1)
+                where (a, s) = st x
 
 info :: ST a -> State -> a
 info st x = (\(a, s) -> a) (app st x)
@@ -18,7 +23,7 @@ instance Functor ST where
                         in (g x, s'))
 
 instance Applicative ST where
-  pure x      = S (\s -> (x, s))
+  pure x      = S (\s -> (x, s - 1))
   stf <*> stx = S (\s ->  let (f, s')   = app stf s
                               (x, s'')  = app stx s'
                               in (f x, s''))
