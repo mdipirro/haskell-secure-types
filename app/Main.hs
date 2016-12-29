0e70e2f149b5b85ebff92599e298f31629efcacb
mdipirro <matteodipirro@gmail.com>
2016-12-29 19:55:14 +0100

    Testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,13 @@
 import Src.Model.Employee
 import Src.IO.JSONHandler
 import Security.Unsecure
-import Security.Taint
-
-import Data.List (partition)
+import Security.UserTaint
 
 main :: IO ()
 main = do employees <- getEmployees
-          let employees'  = transform employees (\es -> map (increaseSalary 200) es)
+          let employees'  = umap employees (\es -> map (increaseSalary 200) es)
               e           = validate employees
               t           = pure (\x y -> x + y) <*> pure 10 <*> pure 20 :: Taint Int
               t'          = pure (\x y z -> x + y + z) <*> pure 30 <*> t <*> pure 10
-          print $ get t'
+          print $ getFromConfig t'
+          print $ getFromInt t' 10
