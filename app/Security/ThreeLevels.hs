68a4eca02f07ab56d3396f8dc2b80d85dea030aa
mdipirro <matteodipirro@gmail.com>
2017-01-01 17:25:55 +0100

    Add Lattice instance

---
+++ app/Security/ThreeLevels.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module Security.ThreeLevels (Low, Medium, High, low, medium, high) where
+
+import Security.Lattice
+
+data Low    = L
+data Medium = M
+data High   = H
+
+type instance (LEQ Low Low) = ()
+type instance (LEQ Low Medium) = ()
+type instance (LEQ Medium Medium) = ()
+type instance (LEQ Medium High) = ()
+type instance (LEQ High High) = ()
+
+low :: Ticket Low
+low = Ticket
+
+medium :: Ticket Medium
+medium = Ticket
+
+high :: Ticket High
+high = Ticket
