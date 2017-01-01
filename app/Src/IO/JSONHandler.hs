3001ebda31348b36cfc0419e3400ea496c6b8b92
mdipirro <matteodipirro@gmail.com>
2017-01-01 17:26:30 +0100

    Add SecureFlow

--- app/Src/IO/JSONHandler.hs
+++ app/Src/IO/JSONHandler.hs
@@ -1,22 +1,36 @@
-module Src.IO.JSONHandler (getEmployees, saveEmployees) where
+module Src.IO.JSONHandler (getEmployees, saveEmployees, getCredentials) where
 
 import Data.Aeson
 import Data.Aeson.Encode.Pretty
 import qualified Data.ByteString.Lazy as BSL
 import Security.Unsecure
+import Security.SecureFlow
+import Security.ThreeLevels
 import Src.Model.Employee
+import Src.Model.Credential
+import Src.Model.Store
 import qualified Src.Validation.Employee as EmployeeV
 
 employeesFile :: FilePath
 employeesFile = "data/employees.json"
 
-readEmployeesFile :: IO BSL.ByteString
-readEmployeesFile = BSL.readFile employeesFile
+storesFile :: FilePath
+storesFile = "data/stores.json"
+
+credentialsFile :: FilePath
+credentialsFile = "data/credentials.json"
+
+--readEmployeesFile :: IO BSL.ByteString
+--readEmployeesFile = BSL.readFile employeesFile
 
 saveEmployees es = BSL.writeFile employeesFile (encodePretty es)
 
 getEmployees :: IO (Unsecure [Employee] EmployeeV.EmployeeError)
-getEmployees =  do  json <- eitherDecode <$> readEmployeesFile
-                    case json of
-                      Left err -> return (upure [] [])
-                      Right es -> return (upure es (EmployeeV.validate))
+getEmployees =  do  json <- eitherDecode <$> BSL.readFile employeesFile
+                    case json of  Left err -> return (upure [] [])
+                                  Right es -> return (upure es (EmployeeV.validate))
+
+getCredentials :: IO (SecureFlow High [Credential])
+getCredentials = do json <- eitherDecode <$> BSL.readFile credentialsFile
+                    case json of  Left err -> return (fail "")
+                                  Right cs -> return (pure cs)
