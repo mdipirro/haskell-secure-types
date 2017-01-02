37bdac879c0e3d77acdc205cbd906460185405b9
mdipirro <matteodipirro@gmail.com>
2017-01-03 00:28:24 +0100

    Introduce SecureComputation

--- app/Src/IO/TestIOFunctions.hs
+++ app/Src/IO/TestIOFunctions.hs
@@ -1,12 +1,23 @@
 module Src.IO.TestIOFunctions where
 
+import Data.Char
 import Security.Unsecure
+import Security.SecureComputation
 
-data TestIOError =  NegativeNumber
+data TestIOError =  NegativeNumber | NonNumeric
                 deriving Show
 
-getNat :: IO (Unsecure Int TestIOError)
+getNat :: IO (Unsecure String TestIOError)
 getNat = do n <- getLine
-            return $ upure (read n) [(\n ->  if n >= 0
-                                            then Nothing
-                                            else Just NegativeNumber)]
+            return $ upure n   [
+                                  (\s ->  if null $ dropWhile isDigit s
+                                          then Nothing
+                                          else Just NonNumeric),
+                                  (\n ->  if read n >= 0
+                                          then Nothing
+                                          else Just NegativeNumber)
+                                ]
+
+getUnpureNat :: IO (SecureComputation P String)
+getUnpureNat = do n <- getLine
+                  return $ spure n
