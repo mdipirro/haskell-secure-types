833713c3ac0cf0e6b59cb794903a442b45f7c101
mdipirro <matteodipirro@gmail.com>
2017-01-02 11:51:59 +0100

    Continue application

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 import Src.Model.Employee
+import qualified Src.Model.SecureEmployee as SE
 import Src.Model.Credential
 import Src.IO.JSONHandler
+import Src.IO.TestIOFunctions
 import Security.SecureFlow
 import Security.ThreeLevels
 import Security.Unsecure
+import Data.List
 
 login :: String -> String -> Hatch High [Credential] Bool
 login e p = pure (\cs -> Just $ elem (Credential e p) cs)
@@ -19,14 +22,55 @@ askForLogin cs = do putStr "Email: "
                                     _          -> do  putStr "Incorrect credentials, please try again\n"
                                                       askForLogin cs
 
+hatchSalary :: Int -> Hatch High Int Int
+hatchSalary i = pure (\s -> Just $ s+i)
+
+incSalary :: Int -> String -> [SE.SEmployee] -> [SE.SEmployee]
+incSalary _ _ []       =  []
+incSalary i t (se:ses) =  if t == ((\(Just a) -> a) $ open medium $ SE.email se)
+                          then (SE.SEmployee f l b s e le):ses
+                          else incSalary i t ses
+                          where f = SE.firstName se
+                                l = SE.lastName se
+                                b = SE.birthdate se
+                                s = (declassifyWith (hatchSalary i) (SE.salary se)) :: SecureFlow High Int
+                                e = SE.email se
+                                le = SE.leader se
+
+showSalary :: Hatch High Int Int
+showSalary = pure (\s -> Just s)
+
+showEmployeeSalary :: String -> [SE.SEmployee] -> IO (Maybe Int)
+showEmployeeSalary _ []       = return Nothing
+showEmployeeSalary n (se:ses) = do  if n == ((\(Just a) -> a) $ open medium $ SE.email se)
+                                    then return $ open low $ ((declassifyWith showSalary (SE.salary se)) :: SecureFlow Low Int)
+                                    else showEmployeeSalary n ses
+
+menu :: [SE.SEmployee] -> IO ()
+menu se = do  putStr $ "0) Exit \n"
+              putStr $ "1) See employees' public details \n"
+              putStr $ "2) Increase an employee's salary \n"
+              putStr $ "What do you want to do? "
+              c <- getLine
+              case c of "0" ->  return ()
+                        "1" ->  do  putStr $ intercalate "\n\n" $ map (SE.viewPublicDetails medium) se
+                                    putStr "\n\n"
+                                    menu se
+                        "2" ->  do  putStr $ "Enter the employee's email address: "
+                                    t <- getLine
+                                    putStr $ "Enter the increase: "
+                                    i <- getNat
+                                    case validate i of  Left vi ->  do  let se' = incSalary vi t se
+                                                                        s <- showEmployeeSalary t se'
+                                                                        putStr $ "The new salary is " ++ (show s)
+                                                                        menu se'
+                                                        Right e   -> do print e
+                                                                        menu se
+
+
 main :: IO ()
-main = do cs <-getCredentials
-          e <- askForLogin cs
-          putStr $ "Welcome, " ++ e ++ "\n"
-          putStr $ "What do you want to do? \n"
-          putStr $ "1) See employees \n"
-          c <- getLine
-          case c of "1" -> do ue <- getEmployees
-                              case validate ue of
-                                Left es     -> print es
-                                Right [err] -> print $ "There are some inconsistences " ++ (show err)
+main = do {-cs <-getCredentials
+          e <- askForLogin cs-}
+          se <- getSecureEmployees
+          --putStr $ "Welcome, " ++ e ++ "\n"
+          menu se
